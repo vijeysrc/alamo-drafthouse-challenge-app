c7d1927a6649c20d45ef71bbb68730b7e514fbe3
Vijey Narayanaswamy <vijeysrc@gmail.com>
2022-02-11 09:16:00 +0530

    refactor: move interfaces to its own file

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,32 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
+import {
+  ICity,
+  ICityResponse
+} from '../interfaces'
 
 const getBaseUrl = (city: String) => '/assets/data/mock.json' // `https://drafthouse.com/s/mother/v1/page/market/main/${city}`
-
-interface ICinema {
-  name: string;
-}
-
-interface IMarket {
-  cinemas: ICinema[]
-}
-
-interface IFilm {
-  title: string;
-  slug: string;
-}
-
-interface ISession {
-  sessionId: string;
-  cinemaId: string;
-}
-
-interface ICity {
-  market: IMarket;
-  films: IFilm[];
-  sessions: ISession[]
-}
 
 @Injectable({
   providedIn: 'root'
@@ -39,10 +19,10 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getCinemas(city: String = 'austin'): Observable<any> {
+  getCinemas(city: String = 'austin'): Observable<ICity> {
     const url = getBaseUrl(city)
-    return this.http.get(url).pipe(map((res) => {
-      const { data } = res as {data: ICity}
+    return this.http.get(url).pipe(map(res => {
+      const { data } = res as ICityResponse
       const { market, films, sessions} = data
       const { cinemas } = market
 
---
+++ src/interfaces.ts
@@ -0,0 +1,40 @@
+interface ICinema {
+  name: string;
+}
+
+interface IMarket {
+  cinemas: ICinema[]
+}
+
+interface IFilm {
+  title: string;
+  slug: string;
+}
+
+interface ISession {
+  sessionId: string;
+  cinemaId: string;
+}
+
+interface ICityResponse {
+  data: {
+    market: IMarket;
+    films: IFilm[];
+    sessions: ISession[];
+  }
+}
+
+interface ICity {
+  cinemas: ICinema[];
+  films: IFilm[];
+  sessions: ISession[]
+}
+
+export {
+  ICinema,
+  ICity,
+  ICityResponse,
+  IFilm,
+  IMarket,
+  ISession
+}
